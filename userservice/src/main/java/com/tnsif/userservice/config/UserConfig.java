cb0c43b44b8a1a949bb36a8e713a58145726154f
devadharshinibaskar <devadharshini.dpy@gmail.com>
2025-11-09 08:14:49 +0530

    COMMIT

--- userservice/src/main/java/com/tnsif/userservice/config/UserConfig.java
+++ userservice/src/main/java/com/tnsif/userservice/config/UserConfig.java
@@ -1,14 +1,25 @@
 package com.tnsif.userservice.config;
 
-import org.springframework.boot.web.server.ConfigurableWebServerFactory;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class UserConfig implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+@CrossOrigin(origins = "http://localhost:3000") 
+public class UserConfig {
 
-    @Override
-    public void customize(ConfigurableWebServerFactory factory) {
-                factory.setPort(8080);
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
     }
 }
